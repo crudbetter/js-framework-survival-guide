ece45694c982497ee656628ef0f272e00c728456
Mike Godfrey <mjgodfrey83@gmail.com>
2014-12-10 17:19:43 +0000

    Quick karma, jasmine setup with arbitary failing test

---
+++ test/survivalSpec.js
@@ -0,0 +1,12 @@
+describe('survival guide', function() {
+
+	beforeEach(function() {
+	});
+
+	afterEach(function() {
+	});
+
+	it('should fail', function() {
+		expect(true).toBeFalsy();
+	});
+});
