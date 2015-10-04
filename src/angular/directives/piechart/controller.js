397ac2b80e7dbda940a20173a100cbb31da4cf78
Mike Godfrey <mjgodfrey83@gmail.com>
2015-10-04 16:31:48 +0100

    adapted angular piechart to use arc domain

--- src/angular/directives/piechart/controller.js
+++ src/angular/directives/piechart/controller.js
@@ -1,23 +1,8 @@
+var Arc = require('../../../domain/arc');
+
 module.exports = function($scope, $attrs, piechartConfig) {
   
   var slices;
-  var getArc = function(startAngle, endAngle) {
-    function convertToRadians(angle) {
-      return angle * (Math.PI / 180);
-    };
-
-    function getPointOnCircle(angle) {
-      return {
-        x: 1 * Math.cos(angle),
-        y: 1 * Math.sin(angle)
-      };
-    };
-
-    return {
-      start: getPointOnCircle(convertToRadians(startAngle)),
-      end: getPointOnCircle(convertToRadians(endAngle))
-    };
-  };
 
   this.slices = slices = [];
 
@@ -31,20 +16,17 @@ module.exports = function($scope, $attrs, piechartConfig) {
 
   this.setArcs = function() {
     var prevStartAngle = 0;
-    var totalValue = 0;
+    var totalValue = slices.reduce(function(total, slice) {
+      return total + slice.value;
+    }, 0);
 
     $scope.radius = angular.isDefined($attrs.radius) ? $scope.$eval($attrs.radius) : piechartConfig.radius;
 
     angular.forEach(slices, function(slice) {
-      totalValue += slice.value;
-    });
+      var startAngle = prevStartAngle;
+      var endAngle = prevStartAngle = (prevStartAngle + (360 / (totalValue / slice.value))) % 360;
 
-    angular.forEach(slices, function(slice) {
-      slice.arc = getArc(
-        prevStartAngle,
-        prevStartAngle = (prevStartAngle + (360 / (totalValue / slice.value))) % 360
-      );
-      slice.arc.large = slice.value > (totalValue / 2);
+      slice.arc = new Arc(startAngle, endAngle);
     });
   };
 };
